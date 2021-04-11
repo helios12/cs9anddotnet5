b0e974e52573fcc5983f0772b5b0cca6c7b9423d
helios12 <regawq@gmail.com>
2021-04-11 09:33:53 +0200

    Work in progress on chapter 03.

--- Chapter03/SelectionStatements/Program.cs
+++ Chapter03/SelectionStatements/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+
+using static System.Console;
 
 namespace SelectionStatements
 {
@@ -6,7 +9,101 @@ namespace SelectionStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length == 0)
+            {
+                WriteLine("There are no arguments.");
+            }
+            else
+            {
+                WriteLine($"There are {args.Length} arguments.");
+            }
+
+            object o = 3;
+            int j = 4;
+            if (o is int i)
+            {
+                WriteLine($"{i} x {j} = {i * j}");
+            }
+            else
+            {
+                WriteLine("o is not an int so it cannot multiply.");
+            }
+
+            A_label:
+            var number = (new Random()).Next(1, 7);
+            WriteLine($"My random number is {number}.");
+            switch (number)
+            {
+                case 1:
+                    WriteLine("One");
+                    break;
+                case 2:
+                    WriteLine("Two");
+                    goto case 1;
+                case 3:
+                case 4:
+                    WriteLine("Three or four");
+                    goto case 1;
+                case 5:
+                    System.Threading.Thread.Sleep(500);
+                    goto A_label;
+                default:
+                    WriteLine("Default");
+                    break;
+            }
+
+            string path = @"C:\Users\DmitriyGolubitskiy\Documents\repos\cs9anddotnet5\Chapter03\";
+            Write("Press R for readonly or W for write: ");
+            ConsoleKeyInfo key = ReadKey();
+            WriteLine();
+            Stream s = null;
+            if (key.Key == ConsoleKey.R)
+            {
+                s = File.Open(
+                    Path.Combine(path, "file.txt"),
+                    FileMode.OpenOrCreate,
+                    FileAccess.Read
+                );
+            }
+            else if (key.Key == ConsoleKey.W)
+            {
+                s = File.Open(
+                    Path.Combine(path, "file.txt"),
+                    FileMode.OpenOrCreate,
+                    FileAccess.Write
+                );
+            }
+            string message = string.Empty;
+            switch (s)
+            {
+                case FileStream writeableFile when s.CanWrite:
+                    message = "The stream is a file that I can write to.";
+                    break;
+                case FileStream readOnlyFile:
+                    message = "The stream is a readonly file.";
+                    break;
+                case MemoryStream memoryStream:
+                    message = "The stream is a memory address.";
+                    break;
+                default:
+                    message = "The stream is some other type.";
+                    break;
+                case null:
+                    message = "The stream is null.";
+                    break;
+            }
+            WriteLine(message);
+
+            message = s switch
+            {
+                FileStream writeableFile when s.CanWrite => "The stream is a file that I can write to!",
+                FileStream readonlyFile => "The stream is a readonly file!",
+                MemoryStream memoryStream => "The stream is a memory address!",
+                null => "The stream is null!",
+                _ => "The stream is some other type!"
+            };
+            WriteLine(message);
+
         }
     }
 }
