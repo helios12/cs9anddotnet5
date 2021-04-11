63dd8f885f3724772d2d73668be2678d947dca0f
helios12 <regawq@gmail.com>
2021-04-11 11:32:37 +0200

    Work in progress on chapter 03.

---
+++ Chapter03/CastingConverting/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+using static System.Console;
+using static System.Convert;
+
+namespace CastingConverting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            double b = a;
+            WriteLine(b);
+
+            double c = 9.8;
+            int d = (int)c;
+            WriteLine(d);
+
+            long e = 10;
+            int f = (int)e;
+            WriteLine($"e is {e:N0} and f is {f:N0}");
+            e = 5_000_000_000;
+            f = (int)e;
+            WriteLine($"e is {e:N0} and f is {f:N0}");
+
+            double g = 9.8;
+            int h = ToInt32(g);
+            WriteLine($"g is {g} and h is {h}");
+
+            double[] doubles = {9.49, 9.5, 9.51, 10.49, 10.5, 10.51};
+            foreach (double n in doubles)
+            {
+                WriteLine($"ToInt32({n}) is {ToInt32(n)}");
+            }
+
+            foreach (double n in doubles)
+            {
+                WriteLine(format:
+                    "Math.Round({0}, 0, MidpointRounding.AwayFromZero) is {1}",
+                    arg0: n,
+                    arg1: Math.Round(n, 0, MidpointRounding.AwayFromZero)
+                );
+            }
+
+            foreach (double n in doubles)
+            {
+                WriteLine(format:
+                    "Math.Round({0}, 0, MidpointRounding.ToZero) is {1}",
+                    arg0: n,
+                    arg1: Math.Round(n, 0, MidpointRounding.ToZero)
+                );
+            }
+        }
+    }
+}
--- Chapter03/IterationStatements/Program.cs
+++ Chapter03/IterationStatements/Program.cs
@@ -8,7 +8,42 @@ namespace IterationStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x = 0;
+            while (x < 10)
+            {
+                WriteLine(x);
+                x++;
+            }
+
+            // string password = string.Empty;
+            // string correctPassword = "password";
+            // int maxCount = 5, currentCount = 0;
+            // do
+            // {
+            //     currentCount++;
+            //     Write($"Enter your password [{currentCount}]: ");
+            //     password = ReadLine();
+            // } while (password != correctPassword && currentCount < maxCount);
+            // if (password == correctPassword)
+            // {
+            //     WriteLine("Correct!");
+            // }
+            // else
+            // {
+            //     WriteLine("You have exceeded the number of attempts!");
+            // }
+
+            for (int i = 1; i <= 10; i++)
+            {
+                WriteLine(i);
+            }
+
+            string[] names = {"Adam", "Barry", "Charlie"};
+            foreach (string name in names)
+            {
+                WriteLine($"{name} has {name.Length} characters.");
+            }
         }
+
     }
 }
