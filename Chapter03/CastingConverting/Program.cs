30bb878a444b3dc970078414a3ff03c6f9c02b19
helios12 <regawq@gmail.com>
2021-04-11 15:35:16 +0200

    Work in progress on chapter 03.

--- Chapter03/CastingConverting/Program.cs
+++ Chapter03/CastingConverting/Program.cs
@@ -51,6 +51,44 @@ namespace CastingConverting
                     arg1: Math.Round(n, 0, MidpointRounding.ToZero)
                 );
             }
+
+            int number = 12;
+            WriteLine(number.ToString());
+            bool boolean = true;
+            WriteLine(boolean.ToString());
+            DateTime now = DateTime.Now;
+            WriteLine(now.ToString());
+            object me = new object();
+            WriteLine(me);
+
+            byte[] binaryObject = new byte[128];
+            (new Random()).NextBytes(binaryObject);
+            WriteLine("Binary object as bytes:");
+            for (int i = 0; i < binaryObject.Length; i++)
+            {
+                Write($"{binaryObject[i]:X} ");
+            }
+            WriteLine();
+            string encoded = Convert.ToBase64String(binaryObject);
+            WriteLine($"Binary object as Base64: {encoded}");
+
+            int age = int.Parse("39");
+            DateTime birthday = DateTime.Parse("12 May 1981");
+            WriteLine($"I was born {age} years ago.");
+            WriteLine($"My birthday is {birthday}.");
+            WriteLine($"My birthday is {birthday:D}.");
+
+            Write("How many eggs are there? ");
+            int count;
+            string input = ReadLine();
+            if (int.TryParse(input, out count))
+            {
+                WriteLine($"There are {count} eggs.");
+            }
+            else
+            {
+                WriteLine("I could not parse the input!");
+            }
         }
     }
 }
