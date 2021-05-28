583b22b38fe434ba0716036c7a8f09aa8d957ac0
helios12 <regawq@gmail.com>
2021-05-28 17:35:22 +0200

    Work in progress on Chapter 11.

---
+++ Chapter11/WorkingWithEFCore/Northwind.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Packt.Shared
+{
+    public class Northwind : DbContext
+    {
+        public DbSet<Category> Categories {get; set;}
+        public DbSet<Product> Products {get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string path = System.IO.Path.Combine(System.Environment.CurrentDirectory, "Northwind.db");
+            optionsBuilder.UseSqlite($"Filename={path}");
+        }
+
+        
+    }
+}
